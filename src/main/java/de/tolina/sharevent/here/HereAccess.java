c8369cc3a0f206ca3f0f752f106e0e2744f3d334
Steven Busack <steven.busack@tolina.de>
2018-08-18 14:40:04 +0200

    HERE Zugriffsdaten hinzugefügt.

---
+++ src/main/java/de/tolina/sharevent/here/HereAccess.java
@@ -0,0 +1,18 @@
+package de.tolina.sharevent.here;
+
+/**
+ * HERE APIs use two random strings, app_id and app_code, to provide secure access to services.<br/>
+ * Source: https://developer.here.com/faqs#access-control
+ */
+class HereAccess {
+
+	/**
+	 * Uniquely identifies your application.
+	 */
+	public static final String APP_ID = "adMxD537gzyISZEteKBd";
+
+	/**
+	 * Used in the authentication process to identify a session.
+	 */
+	public static final String APP_CODE = "LFkbh90UNOfoVzUpW84RYA";
+}
