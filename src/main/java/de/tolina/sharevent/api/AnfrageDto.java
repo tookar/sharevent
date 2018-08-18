c0372d3e3ab842d818973c476f15e45c1ad77d1f
Christopher Ewest <christopher.ewest@arxes-tolina.de>
2018-08-18 15:13:40 +0200

    Beispiel Controller

--- src/main/java/de/tolina/sharevent/api/AnfrageDto.java
+++ src/main/java/de/tolina/sharevent/api/AnfrageDto.java
@@ -1,8 +1,14 @@
 package de.tolina.sharevent.api;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(NON_NULL)
 @Data
 public class AnfrageDto {
 
--- src/main/java/de/tolina/sharevent/api/AusgabeDto.java
+++ src/main/java/de/tolina/sharevent/api/AusgabeDto.java
@@ -1,4 +1,14 @@
 package de.tolina.sharevent.api;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(NON_NULL)
+@Data
 public class AusgabeDto {
 }
