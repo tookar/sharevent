82a3fa9089b02fae00b341a5a08acd2861543907
Christopher Ewest <christopher.ewest@arxes-tolina.de>
2018-08-18 14:26:56 +0200

    Initial commit

---
+++ src/main/java/de/tolina/sharevent/ShareventApplication.java
@@ -0,0 +1,12 @@
+package de.tolina.sharevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareventApplication.class, args);
+	}
+}
