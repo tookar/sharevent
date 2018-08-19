9b7c8b200abf86dc3e7b1a937d99dc1ba60ea050
Christopher Ewest <christopher.ewest@arxes-tolina.de>
2018-08-19 08:51:31 +0200

    release

--- src/main/java/de/tolina/sharevent/api/StringListDto.java
+++ src/main/java/de/tolina/sharevent/api/StringListDto.java
@@ -2,9 +2,15 @@ package de.tolina.sharevent.api;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(NON_NULL)
 public class StringListDto {
 	private List<String> strings;
 }
